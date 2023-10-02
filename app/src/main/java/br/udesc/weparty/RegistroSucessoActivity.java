950e2e2dd85022bee550f02ee03e13e64fd2efa1
Daniel Larion Klug <dklug22@gmail.com>
2023-10-02 12:41:26 -0300

    Telas iniciais

--- app/src/main/java/br/udesc/weparty/RegistroSucessoActivity.java
+++ app/src/main/java/br/udesc/weparty/RegistroSucessoActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginRegisterActivity extends AppCompatActivity {
+public class RegistroSucessoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_register);
+        setContentView(R.layout.activity_registro_sucesso);
     }
 }
