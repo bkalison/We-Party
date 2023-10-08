71c599775bfa0d27e6db27da272922af75e60f9d
Alison Berkenbrock <alisonbk172@gmail.com>
2023-10-08 18:42:32 -0300

    Add Intent Login activity
    RegistroSucessoActivity -> LoginActivity

--- app/src/main/java/br/udesc/weparty/RegistroSucessoActivity.java
+++ app/src/main/java/br/udesc/weparty/RegistroSucessoActivity.java
@@ -1,6 +1,9 @@
 package br.udesc.weparty;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,17 @@ public class RegistroSucessoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_sucesso);
+        Button btnLogarAbrir = findViewById(R.id.btnLogarAbrir);
+
+        btnLogarAbrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent();
+            }
+        });
+    }
+    public void intent(){
+        Intent intent = new Intent(RegistroSucessoActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
