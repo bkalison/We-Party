3e43fd75841e850d98c0a9a75e23bc95c23fb8bf
Alison Berkenbrock <alisonbk172@gmail.com>
2023-10-08 17:13:46 -0300

    Login

--- app/src/main/java/br/udesc/weparty/LoginActivity.java
+++ app/src/main/java/br/udesc/weparty/LoginActivity.java
@@ -2,13 +2,55 @@ package br.udesc.weparty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "PREFS_FILE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        EditText editTextPassword = findViewById(R.id.editTextPassword);
+        Button btnRegistrar = findViewById(R.id.btnRegistrar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Por favor preencha os campos !", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (checkCredentials(email, password)) {
+                        Toast.makeText(LoginActivity.this, "Login feito com sucesso", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Email ou senha Incorreta", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
+
+    private boolean checkCredentials(String email, String password) {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        String savedEmail = sharedPreferences.getString("email", "");
+        String savedPassword = sharedPreferences.getString("password", "");
+
+        return email.equals(savedEmail) && password.equals(savedPassword);
+    }
+
+
+
 }
