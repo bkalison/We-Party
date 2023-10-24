283673840bd5ffacdfa6372375eb8daa5d780990
Daniel Larion Klug <dklug22@gmail.com>
2023-10-23 21:55:32 -0300

    Continuação das telas

--- app/src/main/java/br/udesc/weparty/ui/feed/FeedFragment.java
+++ app/src/main/java/br/udesc/weparty/ui/feed/FeedFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -23,9 +22,6 @@ public class FeedFragment extends Fragment {
 
         binding = FragmentFeedBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textFeed;
-        feedViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
