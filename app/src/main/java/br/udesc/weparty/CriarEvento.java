e13065d6fad2070ade22df5225d8cc85b2bbff5c
Alison Berkenbrock <alisonbk172@gmail.com>
2023-11-03 19:11:41 -0300

    create Event (att)
    SALVE MEUS CALABRESO

--- app/src/main/java/br/udesc/weparty/CriarEvento.java
+++ app/src/main/java/br/udesc/weparty/CriarEvento.java
@@ -1,21 +1,30 @@
 package br.udesc.weparty;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
+import br.udesc.weparty.Model.Evento;
+
+
 public class CriarEvento extends AppCompatActivity {
+    FirebaseAuth firebaseAuth;
+    Evento event;
     final Calendar calendar = Calendar.getInstance();
     EditText dataEvento, horaEvento;
     @Override
@@ -66,7 +75,75 @@ public class CriarEvento extends AppCompatActivity {
         });
     }
 
-    private void atualizaData() {
+    public void checkFieldsEvent() {
+        EditText editTextNomeEvento = findViewById(R.id.editTextNomeEvento);
+        EditText editTextDescricao = findViewById(R.id.editTextDescricao);
+        EditText editTextDataDoEvento = findViewById(R.id.editTextDataDoEvento);
+        EditText editTextHoraDoEvento = findViewById(R.id.editTextHoraDoEvento);
+        EditText editTextCep = findViewById(R.id.editTextCep);
+        EditText editTextCidade = findViewById(R.id.editTextCidade);
+        EditText editTextUf = findViewById(R.id.editTextUf);
+        EditText editTextBairro = findViewById(R.id.editTextBairro);
+        EditText editTextRua = findViewById(R.id.editTextRua);
+        EditText editTextNumero = findViewById(R.id.editTextNumero);
+        EditText editTextComplemento = findViewById(R.id.editTextComplemento);
+
+        String nome = editTextNomeEvento.getText().toString();
+        String descricao = editTextDescricao.getText().toString();
+        String data = editTextDataDoEvento.getText().toString();
+        String hora = editTextHoraDoEvento.getText().toString();
+        String cep = editTextCep.getText().toString();
+        String cidade = editTextCidade.getText().toString();
+        String estado = editTextUf.getText().toString();
+        String bairro = editTextBairro.getText().toString();
+        String rua = editTextRua.getText().toString();
+        String numero = editTextNumero.getText().toString();
+        String complemento = editTextComplemento.getText().toString();
+
+        if (TextUtils.isEmpty(nome) || TextUtils.isEmpty(descricao) || TextUtils.isEmpty(data) || TextUtils.isEmpty(hora) || TextUtils.isEmpty(cep) ||
+                TextUtils.isEmpty(cidade) || TextUtils.isEmpty(estado) || TextUtils.isEmpty(bairro) || TextUtils.isEmpty(rua) || TextUtils.isEmpty(numero) || TextUtils.isEmpty(complemento)) {
+            Toast.makeText(this, "Todos os campos são Obrigatorios", Toast.LENGTH_SHORT).show();
+        }
+
+        if (nome.isEmpty()) {
+            Toast.makeText(this, "O campo 'Nome' é obrigatório.", Toast.LENGTH_SHORT).show();
+        } else if (descricao.isEmpty()) {
+            Toast.makeText(this, "O campo 'Descrição' é obrigatório.", Toast.LENGTH_SHORT).show();
+        } else if (data.isEmpty()) {
+            Toast.makeText(this, "O campo 'Data' é obrigatório.", Toast.LENGTH_SHORT).show();
+        } else if (hora.isEmpty()) {
+            Toast.makeText(this, "O campo 'Hora' é obrigatório.", Toast.LENGTH_SHORT).show();
+        } else if (cep.isEmpty()) {
+            Toast.makeText(this, "O campo 'CEP' é obrigatório.", Toast.LENGTH_SHORT).show();
+        } else if (cidade.isEmpty()) {
+            Toast.makeText(this, "O campo 'Cidade' é obrigatório.", Toast.LENGTH_SHORT).show();
+        } else if (estado.isEmpty()) {
+            Toast.makeText(this, "O campo 'Estado' é obrigatório.", Toast.LENGTH_SHORT).show();
+        } else if (bairro.isEmpty()) {
+            Toast.makeText(this, "O campo 'Bairro' é obrigatório.", Toast.LENGTH_SHORT).show();
+        } else if (rua.isEmpty()) {
+            Toast.makeText(this, "O campo 'Rua' é obrigatório.", Toast.LENGTH_SHORT).show();
+        } else if (numero.isEmpty()) {
+            Toast.makeText(this, "O campo 'Número' é obrigatório.", Toast.LENGTH_SHORT).show();
+        } else if (complemento.isEmpty()) {
+            Toast.makeText(this, "O campo 'Complemento' é obrigatório.", Toast.LENGTH_SHORT).show();
+        } else {
+            // Todos os campos estão preenchidos, então crie o evento
+            event = new Evento();
+            event.setName(nome);
+            event.setDescription(descricao);
+            event.setCep(cep);
+            event.setCity(cidade);
+            event.setState(estado);
+            event.setDistrict(bairro);
+            event.setAddress(rua);
+            event.setNumber(numero);
+            event.setComplement(complemento);
+        }
+    }
+
+
+        private void atualizaData() {
         String format = "dd/MM/yyyy";
         SimpleDateFormat dateFormat = new SimpleDateFormat(format, Locale.US);
         dataEvento.setText(dateFormat.format(calendar.getTime()));
@@ -77,5 +154,7 @@ public class CriarEvento extends AppCompatActivity {
         SimpleDateFormat dateFormat = new SimpleDateFormat(format, Locale.US);
         horaEvento.setText(dateFormat.format(calendar.getTime()));
     }
+
+
 
 }
---
+++ app/src/main/java/br/udesc/weparty/Model/Evento.java
@@ -0,0 +1,93 @@
+package br.udesc.weparty.Model;
+
+import java.util.Date;
+
+public class Evento {
+    private String name, address, complement, description, cep, city, state, district, number;
+    private Date date;
+
+    public Evento() {
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
