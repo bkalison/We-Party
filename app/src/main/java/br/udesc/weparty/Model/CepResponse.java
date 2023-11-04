dd54f269f296a8c485aef3818d0864bb1403a7a4
Alison Berkenbrock <alisonbk172@gmail.com>
2023-11-04 20:50:52 -0300

    RETROFIT CEP
    Implementation Cep Retrofit

---
+++ app/src/main/java/br/udesc/weparty/Model/CepResponse.java
@@ -0,0 +1,57 @@
+package br.udesc.weparty.Model;
+
+public class CepResponse {
+    private String cep, logradouro, bairro, localidade, uf;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+
+    @Override
+    public String toString() {
+        return "CepResponse{" +
+                "cep='" + cep + '\'' +
+                ", logradouro='" + logradouro + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", localidade='" + localidade + '\'' +
+                ", uf='" + uf + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/br/udesc/weparty/Model/CepService.java
@@ -0,0 +1,10 @@
+package br.udesc.weparty.Model;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface CepService {
+    @GET("ws/{cep}/json/")
+    Call<CepResponse> buscarCep(@Path("cep") String cep);
+}
