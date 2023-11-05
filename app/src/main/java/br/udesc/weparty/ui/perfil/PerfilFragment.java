5c8ad3ab1f2cfa5a302de5bc44f4947e8735e57d
Alison <alisonbk172@gmail.com>
2023-11-05 20:19:11 -0300

    feat: Add name and email fields to the profile screen

--- app/src/main/java/br/udesc/weparty/ui/perfil/PerfilFragment.java
+++ app/src/main/java/br/udesc/weparty/ui/perfil/PerfilFragment.java
@@ -1,9 +1,14 @@
 package br.udesc.weparty.ui.perfil;
 
+import static br.udesc.weparty.Activity.RegistroActivity.PREFS_NAME;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,12 +22,22 @@ public class PerfilFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         PerfilViewModel perfilViewModel =
                 new ViewModelProvider(this).get(PerfilViewModel.class);
 
         binding = FragmentPerfilBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        String nomeDoUsuario = sharedPreferences.getString("nome", "Nome do Usuário Padrão");
+        String emailDoUsuario = sharedPreferences.getString("email", "usuario@example.com");
+
+        TextView nomeTextView = binding.textNomePerfil;
+        TextView emailTextView = binding.textEmailPerfil;
+
+        nomeTextView.setText(nomeDoUsuario);
+        emailTextView.setText(emailDoUsuario);
         return root;
     }
 
