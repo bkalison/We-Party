91bfc53f3d29fa6e616fe6c788a36c8a60921d95
Pedro Lopes <pedrolopeshls99@gmail.com>
2023-11-29 23:36:39 -0300

    remove mock

--- app/src/main/java/br/udesc/weparty/Activity/MainActivity.java
+++ app/src/main/java/br/udesc/weparty/Activity/MainActivity.java
@@ -22,24 +22,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         (new Handler()).postDelayed(this::startApp, 1000);
         FirebaseApp.initializeApp(this);
-        setMockEventsCards();
     }
 
     private void startApp() {
         startActivity(new Intent(MainActivity.this, LoginRegistroActivity.class));
 //        startActivity(new Intent(MainActivity.this, HomeActivity.class));
-    }
-
-    public void setMockEventsCards() {
-        List<EventCard> eventCards = new ArrayList<>();
-
-        eventCards.add(new EventCard(1, "SEXTA ÀS 1PM", "Evento Teste 1", 10, 10));
-        eventCards.add(new EventCard(2, "SEXTA ÀS 2PM", "Evento Teste 2", 20, 20));
-        eventCards.add(new EventCard(3, "SEXTA ÀS 3PM", "Evento Teste 3", 30, 30));
-        eventCards.add(new EventCard(4, "SEXTA ÀS 4PM", "Evento Teste 4", 40, 40));
-        eventCards.add(new EventCard(5, "SEXTA ÀS 5PM", "Evento Teste 5", 50, 50));
-        eventCards.add(new EventCard(6, "SEXTA ÀS 6PM", "Evento Teste 6", 60, 60));
-        eventCards.add(new EventCard(7, "SEXTA ÀS 7PM", "Evento Teste 7", 70, 70));
-
     }
 }
