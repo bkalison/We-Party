ac39e7f5ff7d7f19ac265a0a977d2e54ab23bfa7
Pedro Lopes <pedrolopeshls99@gmail.com>
2023-12-02 00:48:03 -0300

    feat: add notification tests

--- app/src/main/java/br/udesc/weparty/Activity/MainActivity.java
+++ app/src/main/java/br/udesc/weparty/Activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.FirebaseApp;
@@ -13,15 +14,54 @@ import java.util.List;
 
 import br.udesc.weparty.Model.EventCard;
 import br.udesc.weparty.R;
+import android.util.Log;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.installations.FirebaseInstallations;
+import com.google.firebase.installations.InstallationTokenResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    String TAG = "TOKEN";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         (new Handler()).postDelayed(this::startApp, 1000);
         FirebaseApp.initializeApp(this);
+
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(token -> {
+            if (!TextUtils.isEmpty(token)) {
+                Log.d(TAG, "retrieve token successful : " + token);
+            } else {
+                Log.w(TAG, "token should not be null...");
+            }
+        }).addOnFailureListener(e -> {
+            //handle e
+        }).addOnCanceledListener(() -> {
+            //handle cancel
+        }).addOnCompleteListener(task -> Log.v(TAG, "This is the token : " + task.getResult()));
+
+//        FirebaseMessaging.getInstance().subscribeToTopic("News")
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        String msg = "Done";
+//                        if (!task.isSuccessful()) {
+//                            msg = "Failed";
+//                        }
+//
+//                    }
+//                });
+    }
+
+    public void startService(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startService(intent);
     }
 
     private void startApp() {
